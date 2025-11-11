fc16259a2eeb223fd0a8e9dbb65f1ddf09466d44
Lucas <01lucasgsoares01@gmail.com>
2025-11-11 22:25:08 +0000

    o dominio das imagens é do postimages.org

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'postimages.org',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
